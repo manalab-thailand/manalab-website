b42485b5e03042cf8246cd9ac096b4aa8079dc39
DOUBLEPOR <doublepor.pp$gmail.com>
2020-05-31 20:04:00 +0700

    Add Professor

--- assets/js/member.js
+++ assets/js/member.js
@@ -4,26 +4,26 @@ $(function () {
     $("#MemberTra").hide();
 
     $("#Developer").click(function () {
-        $("#MemberCre").fadeOut(function () {
-            $("#MemberTra").fadeOut(function () {
-                $("#MemberDev").fadeIn();
-            })
+        $("#MemberCre , #MemberTra , #MemberPro").fadeOut("slow" ,function () {
+            $("#MemberDev").fadeIn(1000);
         });
     });
 
     $("#Creative").click(function () {
-        $("#MemberDev").fadeOut(function () {
-            $("#MemberTra").fadeOut(function () {
-                $("#MemberCre").fadeIn();
-            })
+        $("#MemberDev , #MemberTra , #MemberPro").fadeOut("slow" ,function () {
+            $("#MemberCre").fadeIn(1000);
         });
     });
 
     $("#Training").click(function () {
-        $("#MemberDev").fadeOut(function () {
-            $("#MemberCre").fadeOut(function () {
-                $("#MemberTra").fadeIn();
-            })
+        $("#MemberCre , #MemberDev , #MemberPro").fadeOut("slow" ,function () {
+            $("#MemberTra").fadeIn(2100);
+        });
+    });
+
+    $("#Professor").click(function () {
+        $("#MemberCre , #MemberDev , #MemberTra").fadeOut("slow" ,function () {
+            $("#MemberPro").fadeIn(1000);
         });
     });
 });
