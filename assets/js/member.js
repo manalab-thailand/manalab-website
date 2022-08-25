0f3efaf5e88daabd416c1a5552918e2a10bb59ea
kittipan2206 <s6330613017@phuket.psu.ac.th>
2022-08-25 13:21:02 +0700

    add javaScript for generation2 member

--- assets/js/member.js
+++ assets/js/member.js
@@ -1,28 +1,35 @@
 $(function () {
+    $("#MemberDev").hide();
     $("#MemberCre").hide();
     $("#MemberPro").hide();
     $("#MemberTra").hide();
 
     $("#Developer").click(function () {
-        $("#MemberCre , #MemberTra , #MemberPro").fadeOut("slow" ,function () {
+        $("#MemberCre , #MemberTra , #MemberPro , #MemberDev2").fadeOut("slow" ,function () {
             $("#MemberDev").fadeIn(1000);
         });
     });
 
+    $("#Gen2Developer").click(function () {
+        $("#MemberCre , #MemberTra , #MemberPro, #MemberDev").fadeOut("slow" ,function () {
+            $("#MemberDev2").fadeIn(1000);
+        });
+    });
+
     $("#Creative").click(function () {
-        $("#MemberDev , #MemberTra , #MemberPro").fadeOut("slow" ,function () {
+        $("#MemberDev , #MemberTra , #MemberPro, #MemberDev2").fadeOut("slow" ,function () {
             $("#MemberCre").fadeIn(1000);
         });
     });
 
     $("#Training").click(function () {
-        $("#MemberCre , #MemberDev , #MemberPro").fadeOut("slow" ,function () {
+        $("#MemberCre , #MemberDev , #MemberPro , #MemberDev2").fadeOut("slow" ,function () {
             $("#MemberTra").fadeIn(2100);
         });
     });
 
     $("#Professor").click(function () {
-        $("#MemberCre , #MemberDev , #MemberTra").fadeOut("slow" ,function () {
+        $("#MemberCre , #MemberDev , #MemberTra , #MemberDev2").fadeOut("slow" ,function () {
             $("#MemberPro").fadeIn(1000);
         });
     });
