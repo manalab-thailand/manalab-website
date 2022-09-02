26afaa82fb519f28ec4f0ee1d82a778cca4fa3d2
kittipan2206 <s6330613017@phuket.psu.ac.th>
2022-09-02 14:23:10 +0700

    add other button and other member

--- assets/js/member.js
+++ assets/js/member.js
@@ -3,34 +3,42 @@ $(function () {
     $("#MemberCre").hide();
     $("#MemberPro").hide();
     $("#MemberTra").hide();
+    $("#MemberOther").hide();
+    $("#MemberDev2").show();
 
     $("#Developer").click(function () {
-        $("#MemberCre , #MemberTra , #MemberPro , #MemberDev2").fadeOut("slow" ,function () {
-            $("#MemberDev").fadeIn(1000);
+        $("#MemberCre , #MemberTra , #MemberPro , #MemberDev2 ,#MemberOther").fadeOut("slow" ,function () {
+            $("#MemberDev").fadeIn(2000);
         });
     });
 
     $("#Gen2Developer").click(function () {
-        $("#MemberCre , #MemberTra , #MemberPro, #MemberDev").fadeOut("slow" ,function () {
-            $("#MemberDev2").fadeIn(2500);
+        $("#MemberCre , #MemberTra , #MemberPro, #MemberDev ,#MemberOther").fadeOut("slow" ,function () {
+            $("#MemberDev2").fadeIn(2000);
         });
     });
 
     $("#Creative").click(function () {
-        $("#MemberDev , #MemberTra , #MemberPro, #MemberDev2").fadeOut("slow" ,function () {
-            $("#MemberCre").fadeIn(1000);
+        $("#MemberDev , #MemberTra , #MemberPro, #MemberDev2 ,#MemberOther").fadeOut("slow" ,function () {
+            $("#MemberCre").fadeIn(2500);
         });
     });
 
     $("#Training").click(function () {
-        $("#MemberCre , #MemberDev , #MemberPro , #MemberDev2").fadeOut("slow" ,function () {
-            $("#MemberTra").fadeIn(2100);
+        $("#MemberCre , #MemberDev , #MemberPro , #MemberDev2 ,#MemberOther").fadeOut("slow" ,function () {
+            $("#MemberTra").fadeIn(2000);
         });
     });
 
     $("#Professor").click(function () {
-        $("#MemberCre , #MemberDev , #MemberTra , #MemberDev2").fadeOut("slow" ,function () {
-            $("#MemberPro").fadeIn(1000);
+        $("#MemberCre , #MemberDev , #MemberTra , #MemberDev2 ,#MemberOther").fadeOut("slow" ,function () {
+            $("#MemberPro").fadeIn(2000);
+        });
+    });
+
+    $("#Other").click(function () {
+        $("#MemberCre , #MemberDev , #MemberPro , #MemberDev2 ,#MemberTra").fadeOut("slow" ,function () {
+            $("#MemberOther").fadeIn(2000);
         });
     });
 });
